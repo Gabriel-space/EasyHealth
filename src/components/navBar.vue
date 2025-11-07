be331ecdb3f83ac67d62de9ce50029e8adcff364
Gabriel-space <bielneves7070@gmail.com>
2025-11-07 15:17:20 -0300

    editando canto navbar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="navbar bg-neutral text-neutral-content">
+  <div class="navbar bg-neutral text-neutral-content rounded-bl-lg rounded-br-lg shadow-lg mb-2 px-4">
     <div>
-      <button class="btn btn-square btn-ghost" @click="toggleSidebar">
+      <button class="btn btn-square btn-ghost rounded-full" @click="toggleSidebar">
 
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
           class="inline-block h-5 w-5 stroke-current">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
+          <path stroke-linecap="round " stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
         </svg>
       </button>
     </div>
